446dbf25bbdfdaeef56b6ede27ed5501741595c3
perrynzhou <perrynzhou@gmail.com>
2020-11-21 16:50:23 +0800

    update

--- killd/killd.c
+++ killd/killd.c
@@ -4,35 +4,52 @@
   > Mail:perrynzhou@gmail.com 
   > Created Time: Friday, November 20, 2020 PM03:03:33
  ************************************************************************/
+#define _POSIX_SOURCE
 
+#include <linux/kernel.h>
+#include <linux/unistd.h>
+
+#include <linux/module.h>
+#include <linux/init.h>
+
+#include <linux/sched.h>
+#include <linux/cred.h>
 #include <linux/init.h>
 #include <linux/kernel.h> 
 #include <linux/module.h>
-#include <linux/sched.h> 
-#define KILLD_MAX_PROCESS  (1024)
+#include <linux/sched.h>
+#include <linux/syscalls.h>
+#include <linux/signal.h> 
+#include <linux/kernel.h>
+#include <linux/unistd.h>
+#include <linux/cred.h>
 MODULE_LICENSE("BSD");
-static int n_pids;
-static  int pids[KILLD_MAX_PROCESS] ={0};
-module_param_array(pids, int, &n_pids, S_IRUGO);
-
+static  int pid;
+module_param(pid, int,  S_IRUGO);
 static int killd_init(void)
 {
-
   struct task_struct *p;
-  int i = 0;
+  int signum;
+  struct siginfo info;
+  int ret;
   printk(KERN_ALERT "********************welcome to use killd to kill  D status process*************\n");
-  for (i = 0; i < n_pids; i++)
-  {
+  
     for_each_process(p)
     {
-      if (p->pid == pids[i])
+      if (p->pid == pid)
       {
-        printk(KERN_ALERT "killd  %d process success\n",pids[i]);
-        set_task_state(p, EXIT_DEAD);
-        return 0;
+        printk(KERN_ALERT "killd  %d process success\n",pid);
+        set_task_state(p, TASK_INTERRUPTIBLE);
+        signum = SIGKILL;
+        memset(&info, 0, sizeof(struct siginfo));
+        info.si_signo = signum;
+        ret = send_sig_info(signum, &info, p);
+        if (ret < 0) {
+            printk(KERN_INFO "error sending signal\n");
+        }
       }
     }
-  }
+  
   return 0;
 }
 static void killd_exit(void)
@@ -41,6 +58,3 @@ static void killd_exit(void)
 }
 module_init(killd_init);
 module_exit(killd_exit);
-MODULE_AUTHOR("perrynzhou@gmail.com");
-MODULE_DESCRIPTION("kill D status process");
-MODULE_LICENSE("GPL");
